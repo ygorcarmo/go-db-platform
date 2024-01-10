01785c8e43c306254ab756f184558460514723b0
Ygor Carmo <ygor.carmo@gmail.com>
2024-01-10 15:51:00 +1100

    update schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -12,3 +12,8 @@ CREATE TABLE db_connection_info (
     sslMode VARCHAR(50) NOT NULL,
     PRIMARY KEY (name, host)
 );
+
+INSERT INTO db_connection_info (name, host, port, type, sslMode) VALUES
+ ("mysql", "localhost", 3001, "mysql", "disable"),
+ ("maria", "localhost", 3002, "mysql", "disable"),
+ ("postgres", "localhost", 5432, "postgres", "disable");
