bc6e1c02f2d6d1788e1ed0a27ee0107fb73f3e02
Ygor Carmo <ygor.carmo@gmail.com>
2024-03-04 16:01:52 +1100

    add more logic to sign in

--- src/handlers/signIn.go
+++ src/handlers/signIn.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"custom-db-platform/src/views"
+	"fmt"
 	"net/http"
 )
 
@@ -10,5 +11,8 @@ func LoadSignInPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleSignIn(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Printf("Username is %s, Password is %s\n", username, password)
 	w.Write([]byte("Hello"))
 }
